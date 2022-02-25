157e2e8794b1a82aa4faf33b87d4f34e80ac3a37
boboyan <byan@unomaha.edu>
2022-02-25 12:53:27 -0600

    Initial commit

---
+++ burgerwebsite/urls.py
@@ -0,0 +1,5 @@
+from django.urls import include, path
+from .views import index
+urlpatterns = [
+    path('', index, name='products')
+]
---
+++ burgerwebsite/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+# Create your views here.
+from django.http import HttpResponse
+# importing loading from django template
+from django.template import loader
+
+
+# our view which is a function named index
+def index(request):
+    # getting our template
+    template = loader.get_template('products.html')
+    # rendering the template in HttpResponse
+    return HttpResponse(template.render())
