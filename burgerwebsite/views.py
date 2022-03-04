9dee601d071ae4920c6bc601794b0b4a591f2c6c
Induputta <iputta@unomaha.edu>
2022-03-04 09:54:56 -0600

    Updated About us page

--- burgerwebsite/views.py
+++ burgerwebsite/views.py
@@ -8,7 +8,7 @@ from django.template import loader
 # our view which is a function named index
 def index(request):
     # getting our template
-    template = loader.get_template('products.html')
+    template = loader.get_template('aboutus/products.html')
     # rendering the template in HttpResponse
     return HttpResponse(template.render())
 
