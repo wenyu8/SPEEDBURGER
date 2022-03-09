084d79f2d4ecbbd9549a70ca7b02de5336b26a83
BoboYan <byan@unomaha.edu>
2022-03-08 21:03:30 -0600

    Update urls.py

--- burgerwebsite/urls.py
+++ burgerwebsite/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='products'),
     path('aboutus/', views.aboutus, name='aboutus')
+    path('signup/', views.signup, name='signup'),  # added
+
 ]
 
