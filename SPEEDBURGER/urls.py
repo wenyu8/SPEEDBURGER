4080c6cf4d9a83579d8dc138e2b42bcfb59d8b92
Induputta <iputta@unomaha.edu>
2022-03-10 18:59:09 -0600

    Login and logout page

--- SPEEDBURGER/urls.py
+++ SPEEDBURGER/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('burgerwebsite.urls')),
     path(r'^admin/', admin.site.urls),
-    path(r'^burgerwebsite/', include('burgerwebsite.urls'))
-]
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('burgerwebsite.urls')),
+    path(r'^burgerwebsite/', include('burgerwebsite.urls')),
+    # Add Django site authentication urls (for login, logout, password management)
+    path('accounts/', include('django.contrib.auth.urls')),
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
