7d3f1e10745d98f67a544b87b1c38f8862dfcacf
BoboYan <byan@unomaha.edu>
2022-03-19 14:32:10 -0500

    Update urls.py

--- burgerwebsite/urls.py
+++ burgerwebsite/urls.py
@@ -1,13 +1,15 @@
 from . import views
-from django.urls import path
-
+from django.urls import path, re_path, include
+app_name='burgerwebsite'
 urlpatterns = [
     path('', views.index, name='home'),
+    #re_path(r'^home/$', views.index, name='home'),
     path('products/', views.products, name='products'),
     path('aboutus/', views.aboutus, name='aboutus'),
     path('shop/', views.shop, name='shop'),
-    path('signup/', views.signup, name='signup'),  # added
+    path('signup/', views.signup, name='signup'),
     path('contactus/', views.contactus, name='contactus'),
+    #path('', views.homepage, name="homepage"),
 
 ]
 
