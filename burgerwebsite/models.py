dcce37e45aaa18e36443bae645ba7ff0d9679171
Induputta <iputta@unomaha.edu>
2022-03-21 21:53:22 -0500

    Committing updated changes to signup

--- burgerwebsite/models.py
+++ burgerwebsite/models.py
@@ -4,22 +4,23 @@ from django.utils import timezone
 
 class User(models.Model):
 
-    userId = models.IntegerField(primary_key=True)
     firstName = models.CharField(max_length=100, null=True)
     lastName = models.CharField(max_length=100, null=True)
     email = models.CharField(max_length=100, null=True)
     password = models.CharField(max_length=20, null=True)
+    userId = models.IntegerField(primary_key=True)
 
     def __str__(self):
         return self.firstName
 
 
 class Product(models.Model):
+
     productName = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     calories = models.IntegerField()
-    productID = models.IntegerField(max_length=50, primary_key=True)
+    productID = models.IntegerField(primary_key=True)
 
     def __str__(self):
         return str(self.productName)
@@ -28,39 +29,14 @@ class Product(models.Model):
 class OrderDetail(models.Model):
 
     orderId = models.IntegerField(primary_key=True)
-    productID = models.ForeignKey(Product, null=True,on_delete=models.PROTECT)
-    quantity = models.IntegerField(null=True)
-    firstName = models.ForeignKey(User,null=True,on_delete=models.PROTECT)
-    lastName = models.ForeignKey(User,null=True,on_delete=models.PROTECT)
-    email = models.ForeignKey(User,null=True,on_delete=models.PROTECT)
-    status = models.CharField(max_length=20,null=True)
+    productID = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
+    quantity = models.IntegerField()
+    firstName = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users')
+    status = models.CharField(max_length=20, null=True)
     order_timestamp = models.DateTimeField(null=True)
-    amount = models.IntegerField(null=True)
+    amount = models.IntegerField()
     address = models.TextField(null=True)
 
     def __str__(self):
-        return str(self.firstName)
-
-
-class Order(models.Model):
-
-    orderId = models.IntegerField(primary_key=True)
-    productID = models.ForeignKey(OrderDetail, null=True,on_delete=models.PROTECT)
-    orderTotal = models.DecimalField()
-    userId = models.ForeignKey(User,null=True,on_delete=models.PROTECT)
-    email = models.ForeignKey(User,null=True,on_delete=models.PROTECT)
-    status = models.CharField(max_length=20,null=True)
-
-    def __str__(self):
-        return str(self.userId)
-
-
-class Email(models.Model):
-
-    orderId = models.ForeignKey(Order, null=True, on_delete=models.PROTECT)
-    orderTotal = models.ForeignKey(Order, null=True, on_delete=models.PROTECT)
-    userId = models.ForeignKey(User, null=True, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return str(self.userId)
+        return str(self.orderId)
 
--- burgerwebsite/urls.py
+++ burgerwebsite/urls.py
@@ -3,13 +3,13 @@ from django.urls import path, re_path, include
 app_name='burgerwebsite'
 urlpatterns = [
     path('', views.index, name='home'),
-    #re_path(r'^home/$', views.index, name='home'),
+    # re_path(r'^home/$', views.index, name='home'),
     path('products/', views.products, name='products'),
     path('aboutus/', views.aboutus, name='aboutus'),
     path('shop/', views.shop, name='shop'),
     path('signup/', views.signup, name='signup'),
     path('contactus/', views.contactus, name='contactus'),
-    #path('', views.homepage, name="homepage"),
+    # path('', views.homepage, name="homepage"),
 
 ]
 
