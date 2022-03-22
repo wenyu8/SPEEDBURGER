c43aa4d765e766db75033c2b52e76b061f32a146
Induputta <iputta@unomaha.edu>
2022-03-22 00:15:17 -0500

    commit models.py

--- burgerwebsite/models.py
+++ burgerwebsite/models.py
@@ -40,3 +40,24 @@ class OrderDetail(models.Model):
     def __str__(self):
         return str(self.orderId)
 
+
+class Order(models.Model):
+
+    orderId = models.IntegerField(primary_key=True)
+    productID = models.ForeignKey(OrderDetail, on_delete=models.CASCADE, related_name='orderdetails')
+    orderTotal = models.DecimalField(max_digits=10, decimal_places=2)
+    email = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    status = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+        return str(self.userId)
+
+
+class Email(models.Model):
+    email = models.CharField(max_length=100, null=True)
+    orderId = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='orderid')
+    orderTotal = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='orders')
+    userId = models.ForeignKey(User, on_delete=models.CASCADE, related_name='userid')
+
+    def __str__(self):
+        return str(self.userId)
