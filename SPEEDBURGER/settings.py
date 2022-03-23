9d027aeda68d3567fde23d2a711eeb36708ee90f
Induputta <iputta@unomaha.edu>
2022-03-22 19:50:11 -0500

    commit .gitignore

--- SPEEDBURGER/settings.py
+++ SPEEDBURGER/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s&y_(f8y=d2pje%8x%wvh7c%khdl)pl@a%8^4hz4-my22h5%4v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
